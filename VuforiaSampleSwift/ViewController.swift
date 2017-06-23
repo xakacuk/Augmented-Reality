a747c7d07bc0abeac6af3a02113ed102cd21e1b7
Евгений Бейнар <ASky@192.168.0.101.ipl.orionnet.ru>
2017-06-23 20:32:25 +0700

    Initial Commit

---
+++ VuforiaSampleSwift/ViewController.swift
@@ -0,0 +1,277 @@
+//
+//  ViewController.swift
+//  VuforiaSample
+//
+//  Created by Yoshihiro Kato on 2016/07/02.
+//  Copyright © 2016年 Yoshihiro Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let vuforiaLicenseKey = "AdXi9wH/////AAAAGRYmUtRmNktGnWkHUnO95l13T9aaH1ofzoVjBtw/9yu0AH5eFGECsseu9dV4VjSPPTRwy1CE9iGAKBT7Gb0rUDFUTk5UgrVLXJ1ik1LBFfXhlRrrLk0UTRxTev+aKXepTXdpPOISG7dtM7ige/tS0nJkva1PNHUpEXRdVSVLQ4EcIPDa9f4RIm6yZ4PVQo633dwBcjmne21UWHIT0rboFU7gN1rUyRwnzBYSODPfXJHcW8DxL6ADHenoN95HfOOdXXhsHRsMBzzkAafo0f2b+4MfD/wz/eMlYt/HTFNvxR0m+f75yy26uLWLS/4AWVQKw217Ne08YszONHsamftyTPkMmwfWtDogoJMffkig4d9L"
+    
+    let vuforiaDataSetFile = "poprobuem.xml"
+    
+    var vuforiaManager: VuforiaManager? = nil
+    
+    let boxMaterial = SCNMaterial()
+    fileprivate var lastSceneName: String? = nil
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepare()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        do {
+            try vuforiaManager?.stop()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+}
+
+private extension ViewController {
+    func prepare() {
+        vuforiaManager = VuforiaManager(licenseKey: vuforiaLicenseKey, dataSetFile: vuforiaDataSetFile)
+        if let manager = vuforiaManager {
+            manager.delegate = self
+            manager.eaglView.sceneSource = self
+            manager.eaglView.delegate = self
+            manager.eaglView.setupRenderer()
+            self.view = manager.eaglView
+        }
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(didRecieveWillResignActiveNotification),
+                                       name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(didRecieveDidBecomeActiveNotification),
+                                       name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+        
+        vuforiaManager?.prepare(with: .portrait)
+    }
+    
+    func pause() {
+        do {
+            try vuforiaManager?.pause()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+    
+    func resume() {
+        do {
+            try vuforiaManager?.resume()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+}
+
+extension ViewController {
+    func didRecieveWillResignActiveNotification(_ notification: Notification) {
+        pause()
+    }
+    
+    func didRecieveDidBecomeActiveNotification(_ notification: Notification) {
+        resume()
+    }
+}
+
+extension ViewController: VuforiaManagerDelegate {
+    func vuforiaManagerDidFinishPreparing(_ manager: VuforiaManager!) {
+        print("did finish preparing\n")
+        
+        do {
+            try vuforiaManager?.start()
+            vuforiaManager?.setContinuousAutofocusEnabled(true)
+        }catch let error {
+            print("\(error)")
+        }
+    }
+    
+    func vuforiaManager(_ manager: VuforiaManager!, didFailToPreparingWithError error: Error!) {
+        print("did faid to preparing \(error)\n")
+    }
+    
+    func vuforiaManager(_ manager: VuforiaManager!, didUpdateWith state: VuforiaState!) {
+        for index in 0 ..< state.numberOfTrackableResults {
+            let result = state.trackableResult(at: index)
+            let trackerableName = result?.trackable.name
+            //print("\(trackerableName)")
+            
+            if trackerableName == "stones" {
+                boxMaterial.diffuse.contents = UIColor.purple
+                
+                if lastSceneName != "stones" {
+                    manager.eaglView.setNeedsChangeSceneWithUserInfo(["scene" : "stones"])
+                    lastSceneName = "stones"
+                }
+            }
+            
+            if trackerableName == "chips" {
+                boxMaterial.diffuse.contents = UIColor.blue
+                    
+                if lastSceneName != "chips" {
+                    manager.eaglView.setNeedsChangeSceneWithUserInfo(["scene" : "chips"])
+                    lastSceneName = "chips"
+                }
+            }
+            
+            if trackerableName == "ajan" {
+                boxMaterial.diffuse.contents = UIColor.green
+                        
+                if lastSceneName != "ajan" {
+                    manager.eaglView.setNeedsChangeSceneWithUserInfo(["scene" : "ajan"])
+                    lastSceneName = "ajan"
+                }
+            }
+        }
+    }
+}
+
+extension ViewController: VuforiaEAGLViewSceneSource, VuforiaEAGLViewDelegate {
+    
+    func scene(for view: VuforiaEAGLView!, userInfo: [String : Any]?) -> SCNScene! {
+        guard let userInfo = userInfo else {
+            print("default scene")
+            return createStonesScene(with: view)
+        }
+        
+        if let sceneName = userInfo["scene"] as? String , sceneName == "stones" {
+            print("stones scene")
+            return createStonesScene(with: view)
+        } else {
+            if let sceneName = userInfo["scene"] as? String, sceneName == "chips" {
+                print("chips scene")
+                return createChipsScene(with: view)
+            } else {
+                print("ajan scene")
+                return createAjanScene(with: view)
+            }
+        }
+    }
+    
+    fileprivate func createAjanScene(with view: VuforiaEAGLView) -> SCNScene {
+        let scene = SCNScene()
+        
+        boxMaterial.diffuse.contents = UIColor.lightGray
+        
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.light?.color = UIColor.lightGray
+        lightNode.position = SCNVector3(x:0, y:10, z:10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        let example = SCNScene(named: "Crash.dae")
+        
+        let nodes = example?.rootNode.childNodes
+
+        for cn in nodes!{
+            cn.scale = SCNVector3(0.05, 0.05, 0.05)
+            scene.rootNode.addChildNode(cn)
+        }
+        
+        return scene
+    }
+    
+    fileprivate func createChipsScene(with view: VuforiaEAGLView) -> SCNScene {
+        let scene = SCNScene()
+        
+        boxMaterial.diffuse.contents = UIColor.lightGray
+        
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.light?.color = UIColor.lightGray
+        lightNode.position = SCNVector3(x:0, y:10, z:10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        let example = SCNScene(named: "Mario.dae")
+        
+        let nodes = example?.rootNode.childNodes
+        
+        for cn in nodes!{
+            cn.scale = SCNVector3(0.05, 0.05, 0.05)
+            scene.rootNode.addChildNode(cn)
+        }
+
+        return scene
+    }
+    
+    fileprivate func createStonesScene(with view: VuforiaEAGLView) -> SCNScene {
+        let scene = SCNScene()
+        
+        boxMaterial.diffuse.contents = UIColor.lightGray
+        
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.light?.color = UIColor.lightGray
+        lightNode.position = SCNVector3(x:0, y:10, z:10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        let boxNode = SCNNode()
+        boxNode.name = "box"
+        boxNode.geometry = SCNBox(width:2, height:2, length:2, chamferRadius:0.0)
+        boxNode.geometry?.firstMaterial = boxMaterial
+        scene.rootNode.addChildNode(boxNode)
+        
+        return scene
+    }
+
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchDownNode node: SCNNode!) {
+        print("touch down \(node.name ?? "")\n")
+        boxMaterial.transparency = 0.6
+        
+    }
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchUp node: SCNNode!) {
+        print("touch up \(node.name ?? "")\n")
+        boxMaterial.transparency = 1.0
+    }
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchCancel node: SCNNode!) {
+        print("touch cancel \(node.name ?? "")\n")
+        boxMaterial.transparency = 1.0
+    }
+}
+
